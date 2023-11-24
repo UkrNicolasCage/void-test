b2675429761396e2d752e3779a138b1dbbf628e0
UkrNicolasCage <vlaluk352@gmail.com>
2023-11-24 15:42:14 +0200

    fix: Port

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap(): Promise<void> {
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('api', app, document)
 
-    await app.listen(process.env['PORT'] ?? 3000)
+    await app.listen(process.env['PORT'] ?? 4200)
 }
 bootstrap()
